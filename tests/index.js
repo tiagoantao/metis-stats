464399587fe6a5f8f6127dd2f3756ef0736ed350
Tiago Antao <tiagoantao@gmail.com>
2018-02-25 11:56:48 -0700

    initial tests

---
+++ tests/index.js
@@ -0,0 +1,13 @@
+const chai = require('chai')
+const assert = chai.assert
+
+const {create, env} = require('sanctuary')
+const S = create({checkTypes: false, env})
+
+const index = require('../lib/index.js')
+
+
+describe('Counts to frequencies', () => {
+    it('Simple', () => {
+    })
+})
